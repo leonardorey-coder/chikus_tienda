15549cccbc02647030237c37b22d128aa1b976b4
Leonardo Cruz <leonardo.cfjl@gmail.com>
2024-12-22 01:54:56 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,19 +1,16 @@
 <?php
 class Database {
-    private $host = "localhost";
-    private $database_name = "pasteleria_mi_amor";
-    private $username = "root";
-    private $password = "";
+    private $database_url;
     public $conn;
+
+    public function __construct() {
+        $this->database_url = getenv('DATABASE_URL');
+    }
 
     public function getConnection() {
         $this->conn = null;
         try {
-            $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->database_name,
-                $this->username,
-                $this->password
-            );
+            $this->conn = new PDO($this->database_url);
             $this->conn->exec("set names utf8");
         } catch(PDOException $exception) {
             echo "Error de conexión: " . $exception->getMessage();
